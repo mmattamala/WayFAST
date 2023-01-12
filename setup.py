0fde1930614fc2b092d6d6e6a93084d4e098007c
Matias Mattamala <matias@robots.ox.ac.uk>
2023-01-12 17:36:51 +0000

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='wayfast',
       version='1.0',
       description='WayFAST Python module',
       author='Mateus Gasparino',
-      author_email='gward@python.net',
+      author_email='mvalve2@illinois.edu',
       url='https://mateusgasparino.com/wayfast-traversability-navigation/',
-      packages=['wayfast'],
+      packages=find_packages(),
      )
